3cf935c9f2eeca0918e6b4bad7eda49c5d9f8c4f
yvelikov <Powerslave#88>
2017-02-05 10:00:38 +0200

    Uploaded solutions

---
+++ Methods - Lab/03. Printing Triangle/Printing Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintLine();
+        }
+
+        static void PrintLine()
+        {
+
+        }
+    }
+}
---
+++ Methods - Lab/Methods - Lab/Blank Receipt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+
+        static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+    }
+}
